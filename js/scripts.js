a5a7a225953d7e2473476ad4048aad0fd83f44cf
Stefan <s.pavlovic.rs@gmail.com>
2022-04-02 23:40:23 +0200

    Feat: Change color of div on hover to selected one

--- js/scripts.js
+++ js/scripts.js
@@ -16,22 +16,32 @@ function squareGenerator (numberOfSquares) {
         squaresContainer.appendChild(squaresSubContainer);
         for (let j = 0 ; j < numberOfSquares ; j++) {
             const squares = document.createElement('div');
-            squaresSubContainer.appendChild(squares).setAttribute('style', `height:${num};width:${num};`);
+            squaresSubContainer.appendChild(squares).setAttribute('style', `height:${num};width:${num};`)
+            squaresSubContainer.appendChild(squares).classList.add('just-the-square');
         }
 
     }
+    squaresColor = document.querySelectorAll(".just-the-square");
+    squaresColor.forEach(item=>{item.addEventListener('mouseover', ()=>item.setAttribute('style', `height:${num};width:${num};background-color:${currentColor()}`))});
 }
 
 function currentColor (){
-    let color = document.querySelector(".buttons-holder > input").value;
+    let color = document.querySelector(".picker").value;
     return color;
 }
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', ()=>squareGenerator(lastArea));
 
+const range = document.querySelector('.range');
+const size = document.querySelector('.size');
+size.addEventListener('click', ()=>squareGenerator(range.value));
 
+const colorButton = document.querySelector('.color');
+//colorButton.addEventListener('click', ()=>/*add function here */);
 
+let squaresColor = document.querySelectorAll(".just-the-square");
+squaresColor.forEach(item=>{item.addEventListener('click', () => {console.log(1)})});
 
 
 
