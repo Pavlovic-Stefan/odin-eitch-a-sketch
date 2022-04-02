9c8ec52d36d2af60a6d90ee6e08c3b0f6b090368
Stefan <s.pavlovic.rs@gmail.com>
2022-04-03 00:53:26 +0200

    Feat: Rainbow

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ const squaresSubContainer = document.createElement('div')
 const squares = document.createElement('div')
 let lastArea = 16;
 let er = 0;
+let rain = 0;
+let red = 250;
+let green = -50;
+let blue = 0;
+let r = 0;
+let g = 0;
+let b = 0;
 
 function squareGenerator (numberOfSquares) {
     lastArea = numberOfSquares;
@@ -31,6 +38,33 @@ function currentColor (){
     if (er==1){
         return '#ffffff';        
     }
+    if (red>=250){
+        r=1;
+    } else if (red<=0){
+        r=0;
+    }
+    if (green>=250){
+        g=1;
+    } else if (green<=0){
+        g=0;
+    }
+    if (blue>=250){
+        b=1;
+    } else if (blue<=0){
+        b=0;
+    }
+
+    if ( r==1 && b==0 && g==0){green+=50}
+    else if (r==1 && g==1){red-=50}
+    else if (r==0 && g==1 && b==0){blue+=50}
+    else if (g==1 && b==1){green-=50}
+    else if (g==0 && b==1 && r==0){red+=50}
+    else if (r==1 && b==1){blue-=50}
+
+
+    if (rain==1){
+        return `rgb(${red}, ${green}, ${blue})`;
+    }
     return color;
 }
 
@@ -44,13 +78,16 @@ const size = document.querySelector('.size');
 size.addEventListener('click', ()=>squareGenerator(range.value));
 
 const colorButton = document.querySelector('.color');
-colorButton.addEventListener('click', ()=>er=0);
+colorButton.addEventListener('click', ()=>{er=0; rain=0});
 
 let squaresColor = document.querySelectorAll(".just-the-square");
 squaresColor.forEach(item=>{item.addEventListener('click', () => {console.log(1)})});
 
 const eraser = document.querySelector('.eraser');
-eraser.addEventListener('click', ()=>er=1);
+eraser.addEventListener('click', ()=>{er=1; rain=0});
+
+const rainbow = document.querySelector('.rainbow');
+rainbow.addEventListener('click', ()=>{rain=1; er=0});
 
 
 
